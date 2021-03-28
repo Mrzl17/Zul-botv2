701ebfec38a5178ca33fd571ef545af2dce87012
Mrzl17 <75774943+Mrzl17@users.noreply.github.com>
2021-03-28 15:07:43 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+npm install
+npm audit fix
+
+echo "SUDAH SELESAI TINGGAL node index.js SOB:)"
+echo "JANGAN LUPA SU[!]BREK CHANNEL A PROJECT NEW UPDATE:)"
+echo "[!] Bila terjadi error hapus folder node_modules lalu ketik npm i enter npm audit fix enter DONE:)"
